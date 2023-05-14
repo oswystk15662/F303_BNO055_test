e1acb6cd18f59a5599e7790163892d2ed29be2fb
Oyaki122 <oc.yaki122@gmail.com>
2023-05-14 17:43:30 +0900

    printfをfloat対応

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,14 @@
 #include <mbed.h>
 
-int main() {
+asm(".global _printf_float");
+using ThisThread::sleep_for;
 
+int main() {
   // put your setup code here, to run once:
 
-  while(1) {
+  while (1) {
     // put your main code here, to run repeatedly:
+    printf("Hello World!\n");
+    sleep_for(1000);
   }
 }
