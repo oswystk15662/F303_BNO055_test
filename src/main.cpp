e15a8b33bbe6d69941d9e0520bd2f9cd5add0a3d
oswystk15662 <oswystk15662@keio.jp>
2024-08-14 13:38:01 +0900

    なんか値変

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,33 @@
 #include <mbed.h>
+#include <BNO055.hpp>
 
-asm(".global _printf_float");
 using ThisThread::sleep_for;
 
+BNO055 bno(PA_9, PA_10);
+Timer t;
+
 int main() {
-  // put your setup code here, to run once:
+  float degrees[3] = {0};
+  float offset[3] = {0};
+
+  t.start();
+  while(t.read_ms() <= 5000){
+    float data[3];
+    bno.getRadians(data);
+
+    for(int i = 0; i < 3; i++){
+      offset[i] += data[i];
+    }
+  }
+
+  t.stop();
 
   while (1) {
-    // put your main code here, to run repeatedly:
-    printf("Hello World!\n");
-    sleep_for(1000);
+    bno.getDegrees(degrees);
+    printf("x : %d, y : %d, z : %d\n",
+            (int)(degrees[0] - offset[0] * 1),
+            (int)(degrees[1] - offset[1] * 1),
+            (int)(degrees[2] - offset[2] * 1));
+    sleep_for(100);
   }
 }
